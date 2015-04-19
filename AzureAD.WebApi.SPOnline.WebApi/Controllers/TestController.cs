10b959439895eac9b340b8814894829b9b4c8ff6
RARomano <rodrigo@rodrigoromano.net>
2015-04-18 22:19:28 -0300

    Web Api Initial Configuration

---
+++ AzureAD.WebApi.SPOnline.WebApi/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AzureAD.WebApi.SPOnline.WebApi.Controllers
+{
+    [Authorize]
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public string Test()
+        {
+            return "OK";
+        }
+    }
+}
